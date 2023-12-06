34ca467c8ad5c361ceb38c717e36ddc765532986
dieromeo <diegoromeo99@icloud.com>
2023-12-06 16:21:15 +0100

    feat: add title

---
+++ vite-hello/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    AppTitle,
+  }
+};
+</script>
+
+<template>
+  <AppTitle></AppTitle>
+</template>
+
+<style scoped></style>
---
+++ vite-hello/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'main-title',
+    data() {
+        return {
+            titolo: 'La mia prima App con vite!'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ titolo }}</h1>
+</template>
+
+<style>
+h1 {
+    color: bisque;
+}
+</style>
